1c12b12234ebcf49782428a49bd5c0531af961e8
Nazarov <79166357230@yandex.ru>
2020-12-05 08:10:30 +0300

    Добавление CORS

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ app.post('/signin', celebrate({
   }),
 }), login);
 app.post('/signup',
+function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})},
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
@@ -81,9 +83,7 @@ app.use((err, req, res, next) => {
   }
   return res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
 });
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
